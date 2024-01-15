0987f97f07a546e8ef6c6f924cde053fc21ced3b
rodriguesxxx <rodriguesx.daniel89@gmail.com>
2024-01-15 14:27:06 -0400

    refactor: add /api to the root route

--- app/src/main/java/com/danielrodrigues/app/utils/BaseUrlUtil.java
+++ app/src/main/java/com/danielrodrigues/app/utils/BaseUrlUtil.java
@@ -1,9 +1,11 @@
 package com.danielrodrigues.app.utils;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import jakarta.servlet.http.HttpServletRequest;
 
+@Component
 public class BaseUrlUtil {
 
     private static HttpServletRequest request;
@@ -20,7 +22,7 @@ public class BaseUrlUtil {
         String serverName = request.getServerName();
         int serverPort = request.getServerPort();
         String contextPath = request.getContextPath();
-        String url = scheme + "://" + serverName + ":" + serverPort + contextPath;
+        String url = scheme + "://" + serverName + ":" + serverPort + contextPath + "/api";
         return url;
     }
 }
