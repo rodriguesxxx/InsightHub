645fdff9c191cc81652c56ff34570495a7063501
rodriguesxxx <rodriguesx.daniel89@gmail.com>
2024-01-25 16:45:59 -0400

    [DEPLOY] Docker settings | changing database to postgres

--- api/src/main/java/com/danielrodrigues/app/AppApplication.java
+++ api/src/main/java/com/danielrodrigues/app/AppApplication.java
@@ -2,7 +2,6 @@ package com.danielrodrigues.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 public class AppApplication {
